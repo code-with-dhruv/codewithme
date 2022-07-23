33ff8c37e5bebebc84076a7679ae46dbd8b82bc7
dhruv <87694090+code-with-dhruv@users.noreply.github.com>
2022-07-23 17:09:00 +0530

    Update message.py

--- Bot/message.py
+++ Bot/message.py
@@ -1,7 +1,7 @@
 import logging
 from telegram import Bot
 import os
-
+players="-666975064"
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
@@ -19,7 +19,13 @@ def Sendmessage(chat_id, text,reply_markup=None):
     except Exception as e:
         logger.info(e)
         raise UserWarning
-
+def SendMess(players, text,reply_markup=None):
+    try:
+        message = bot.send_message(chat_id=players, text=text, reply_to_message_id=None,parse_mode="HTML", reply_markup=reply_markup)
+        msg_id = message.message_id
+    except Exception as e:
+        logger.info(e)
+        raise UserWarning
 def Editmessage(chat_id, text, msg_id, reply_markup=None):
     try:
         bot.edit_message_text(chat_id=chat_id, text=text, message_id=msg_id,parse_mode="HTML", reply_markup=reply_markup)
